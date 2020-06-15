349e240b49b947ea26d6cf06279ca0cea5c7500e
alair-pires <alair-pires@outlook.com>
2020-06-15 14:36:08 -0300

    Primeiros passos com exceções

--- ByteBank/Program.cs
+++ ByteBank/Program.cs
@@ -10,7 +10,35 @@ namespace ByteBank
     {
         static void Main(string[] args)
         {
+            try
+            {
+                Metodo();
+            }
+            catch(NullReferenceException erro)
+            {
+                Console.WriteLine(erro.Message);
+                Console.WriteLine(erro.StackTrace);
+            }
             Console.ReadLine();
         }
+        
+        public static int Dividir(int numero, int divisor)
+        {
+            ContaCorrente conta = null;
+            Console.WriteLine(conta.Saldo);
+            return numero / divisor;
+        }
+
+        static void Metodo()
+        {
+            TestaDivisao(0);
+        }
+
+        static void TestaDivisao(int divisor)
+        {
+            Dividir(10, divisor);
+        }
+
+
     }
 }
